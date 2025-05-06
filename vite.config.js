1d6a3747c2db5f251061e568a646037ba3e3e727
Roman-Hasan <romanhasan.me@gmail.com>
2025-05-06 07:57:49 +0600

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { resolve } from "path";
+import ( defineConfig } from "vite";
+
+export default define defineConfig({
+   build: {
+      rollupOptions: {
+         input: {
+             main: resolve(__dirname, "index.html"),
+        },
+      },
+   },
+});
